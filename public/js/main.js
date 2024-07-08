553ba213352ddfe50f2e29868b728601a82ae9ea
Davide Dall'Olio <ddallolio@coders51.com>
2024-07-08 15:51:31 +0200

    remove preloader

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,6 @@
 (function ($) {
   "use strict";
 
-  // Preloader
-  $(window).on("load", function () {
-    $("#preloader").delay(600).fadeOut();
-  });
-
   // Mobile Toggle Btn
   $(".navbar-toggle").on("click", function () {
     $("#header").toggleClass("nav-collapse");
